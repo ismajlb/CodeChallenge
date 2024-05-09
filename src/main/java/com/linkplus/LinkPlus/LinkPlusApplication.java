c2100cd7006eeef69165917a0106bbfb3dbd4d1a
Ismailhaki Bina <ismajl.bina@gmail.com>
2024-05-09 13:27:03 +0200

    First commit

---
+++ src/main/java/com/linkplus/LinkPlus/LinkPlusApplication.java
@@ -0,0 +1,13 @@
+package com.linkplus.LinkPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkPlusApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/linkplus/LinkPlus/model/Account.java
@@ -0,0 +1,4 @@
+package com.linkplus.LinkPlus.model;
+
+public class Account {
+}
